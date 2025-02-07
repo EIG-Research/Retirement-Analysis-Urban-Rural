442f874b1cd207a2dd15a321947e78dac98f2a7d
Jiaxin He <jasonhe233@hotmail.com>
2025-02-07 16:00:55 -0500

    Remove redundant code and output graphs

--- Code/3. slice urban-rural retirement.R
+++ Code/3. slice urban-rural retirement.R
@@ -1,5 +1,5 @@
 
-# last update: 01/31/2025 by Jason He
+# last update: 02/04/2025 by Jason He
 
 # Project: Retirement Update 2025; Urban versus Rural
 
@@ -76,51 +76,6 @@ ret_sipp_2023 <- sipp_2023_metro %>%
 ret_sipp_2023 <- ret_sipp_2023 %>%
   mutate(across(c("SHARE_RETIREMENT_ACCESS", "SHARE_MATCHING", "SHARE_PARTICIPATION"), ~1-.x))
 
-####################
-# summarise with respect to education level
-ret_sipp_edu <- sipp_2023_metro %>%
-  
-  # group by education levels and metro/non-metro
-  drop_na(METRO_STATUS, EDUCATION) %>% group_by(METRO_STATUS, EDUCATION) %>%
-  
-  # calculate the four retirement-related statistics for each education level in urban/rural
-  summarise(across(ret_features_weighted, ~ sum(.x)/sum(WPFINWGT))) %>%
-  
-  # calculate size of each education level
-  left_join(sipp_2023_metro %>% count(METRO_STATUS, EDUCATION)) %>%
-  
-  # rename variables
-  rename(AVG_ACCOUNT_SIZE = weighted_TVAL_RET,
-         SHARE_RETIREMENT_ACCESS = weighted_ANY_RETIREMENT_ACCESS,
-         SHARE_MATCHING = weighted_MATCHING,
-         SHARE_PARTICIPATION = weighted_PARTICIPATING,
-         SAMPLE_SIZE = n) %>%
-  
-  # Add overall retirement statistics
-  bind_rows(ret_sipp_2023 %>% mutate(EDUCATION = "Overall")) %>%
-  
-  # arrange by education level
-  arrange(fct_relevel(EDUCATION, 'Overall', 'High School or less', 'Some college', "Bachelor's degree or higher")) %>% 
-  mutate(EDUCATION = factor(EDUCATION, levels = EDUCATION))
-
-# Plot share of workers with retirement plan access by industry
-access_plot_edu <- ggplot(ret_sipp_edu, aes(x = EDUCATION,
-                                            y = SHARE_RETIREMENT_ACCESS,
-                                            fill = METRO_STATUS)) +
-  geom_bar(stat = "identity", position = position_dodge(), alpha = 0.75) +
-  scale_y_continuous(limits = c(0,1), labels = scales::percent) +
-  labs(title = "Retirement Account Access by Education Level",
-       y = "Share of Workers (%)",
-       x = "Education Level",
-       fill = "Metro Status") +
-  theme(plot.title = element_text(hjust = 0.5),
-        axis.text.x = element_text(angle=45, vjust=1, hjust=1))
-
 # save output
 setwd(output_path)
 write.csv(ret_sipp_2023, "urban_rural_retirement.csv", row.names = FALSE)
-write.csv(ret_sipp_edu, "urban_rural_ret_education.csv", row.names = FALSE)
-
-# export graphs
-access_plot_edu
-ggsave("education_level_plot.png")
